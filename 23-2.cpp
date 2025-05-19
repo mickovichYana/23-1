9953c2e7405c420a44182f94eff261b5a9778972
mickovichYana <mickovich14@gmail.com>
2025-05-19 10:05:03 +0500

    Add files via upload

---
+++ 23-2.cpp
@@ -0,0 +1,27 @@
+﻿//TreeFun3.Реализовать для бинарного дерева интерфейс итератора, который будет возвращать
+//значения элементов, находящихся в узлах дерева, в порядке "лево-корень-право".
+//Преобразовывать дерево в список или иную структуру данных нельзя, рекурсию использовать
+//запрещается.
+
+#include "tree_iterator.h"
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    vector<int> elements;
+    Node* root = nullptr;
+    int count = GetValidatedCount();
+    InputElementsAndBuildTree(root, elements, count);
+    PrintInputElements(elements);
+    cout << "\nСтруктура дерева:\n";
+    PrintTree(root);
+    cout << "Обход дерева (левый-корень-правый): ";
+    TreeIterator it(root);
+    while (it.HasNext()) 
+    {
+        cout << it.next() << " ";
+    }
+    cout << "\n";
+    return 0;
+} 
